8a18ddd4a8cdf383dc8283b2074c91d4efb2676e
AdirDahari <AdirDahari@users.noreply.github.com>
2024-03-01 12:19:24 +0200

    update

--- src/database/initDB/post-data.ts
+++ src/database/initDB/post-data.ts
@@ -11,11 +11,61 @@ export const postData = [
       images: [
         {
           url: "https://upload.wikimedia.org/wikipedia/he/5/51/Minecraft_cover.png",
-          alt: "Minecraft image",
+          alt: "Minecraft image1",
         },
         {
           url: "https://wallpapers.com/images/hd/minecraft-background-cfljc4haleghnajo.jpg",
-          alt: "Minecraft image",
+          alt: "Minecraft image2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "xbox",
+    game: {
+      category: ["action", "fps"],
+      name: "Halo",
+      price: 200,
+      description:
+        "Halo is a military science fiction media franchise, originally created and developed by Bungie and currently managed and developed by 343 Industries, part of Microsoft's Xbox Game Studios",
+      productStatus: "used",
+      images: [
+        {
+          url: "https://wallpapers.com/images/featured/halo-infinite-4k-5y2ntgk0d2evtb37.jpg",
+          alt: "Halo image1",
+        },
+        {
+          url: "https://wallpapers.com/images/featured/4k-halo-a24col48qb9cfjei.jpg",
+          alt: "Halo image2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "xbox",
+    game: {
+      category: ["racing", "sport"],
+      name: "forza horizon 5",
+      price: 250,
+      description:
+        "Forza Horizon 5 is a 2021 racing video game developed by Playground Games and published by Xbox Game Studios",
+      productStatus: "like new",
+      images: [
+        {
+          url: "https://m.media-amazon.com/images/I/71LSwnEXpXL._AC_UF1000,1000_QL80_.jpg",
+          alt: "Forza Horizon 5 image1",
         },
       ],
     },
@@ -38,11 +88,69 @@ export const postData = [
       images: [
         {
           url: "https://live.staticflickr.com/65535/50931865918_f513094346_b.jpg",
-          alt: "ratchet and clank image",
+          alt: "ratchet and clank image1",
         },
         {
           url: "https://www.gamereactor.eu/media/11/_3381153.jpg",
-          alt: "ratchet and clank image",
+          alt: "ratchet and clank image2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "playstation",
+    game: {
+      category: ["rpg", "souls like"],
+      name: "demon souls",
+      price: 180,
+      description:
+        "Demon's Souls is a 2020 action role-playing game developed by Bluepoint Games and published by Sony Interactive Entertainment for the PlayStation 5",
+      productStatus: "like new",
+      images: [
+        {
+          url: "https://upload.wikimedia.org/wikipedia/pt/7/78/Demons_Souls_remake_capa.png",
+          alt: "demon souls1",
+        },
+        {
+          url: "https://www.gamereactor.eu/media/38/bluepointvisatupp_3263853.png",
+          alt: "demon souls2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "playstation",
+    game: {
+      category: ["rpg", "action", "adventure"],
+      name: "god of war ragnarök",
+      price: 250,
+      description:
+        "God of War Ragnarök is an action-adventure game developed by Santa Monica Studio and published by Sony Interactive Entertainment",
+      productStatus: "used",
+      images: [
+        {
+          url: "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png",
+          alt: "god of war ragnarök image1",
+        },
+        {
+          url: "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/6zhGBKQpPrlLNI2a7EfALNs1.png",
+          alt: "god of war ragnarök image2",
+        },
+        {
+          url: "https://cdn.vox-cdn.com/thumbor/v8xQnKwKsTTDtjJmAf5efktJueo=/0x0:3464x1914/1200x800/filters:focal(924x636:1478x1190)/cdn.vox-cdn.com/uploads/chorus_image/image/71495129/GOWR_Preview_Screenshot_04.6.png",
+          alt: "god of war ragnarök image3",
         },
       ],
     },
@@ -60,16 +168,143 @@ export const postData = [
       name: "world of warcraft",
       price: 150,
       description:
-        "Blast your way through an interdimensional adventure with Ratchet and Clank",
+        "World of Warcraft (WoW) is a massively multiplayer online role-playing game (MMORPG) released in 2004 by Blizzard Entertainment",
+      productStatus: "like new",
+      images: [
+        {
+          url: "https://img.freepik.com/premium-photo/battle-orcs-paladins-world-warcraft-man-orc-face-face-confrontation-warriors-orcs-men-armor_86390-10174.jpg",
+          alt: "world of warcraft image1",
+        },
+        {
+          url: "https://wallpapers.com/images/hd/world-of-warcraft-1920x1080-ushqf9t9032yk7nk.jpg",
+          alt: "world of warcraft image2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "pc",
+    game: {
+      category: ["fps", "action"],
+      name: "Call of Duty: Modern Warfare III",
+      price: 200,
+      description:
+        "Call of Duty: Modern Warfare III is a 2023 first-person shooter video game developed by Sledgehammer Games and published by Activision",
       productStatus: "new",
       images: [
         {
-          url: "https://wallpapers.com/images/hd/world-of-warcraft-shadowlands-rz06hs1xt9kmnj3k.jpg",
-          alt: "ratchet and clank image",
+          url: "https://cdn.procyber.me/wp-content/uploads/2023/08/423dce31-9d64-4ddb-bf1a-3ee72ad86710.webp",
+          alt: "mw3 image1",
         },
         {
-          url: "https://phongvu.vn/cong-nghe/wp-content/uploads/2020/12/maxresdefault-1-1.jpg",
-          alt: "world of warcraft image",
+          url: "https://cdn.procyber.me/wp-content/uploads/2023/11/0-1.jpg",
+          alt: "mw3 image2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "pc",
+    game: {
+      category: ["puzzle"],
+      name: "portal 2",
+      price: 100,
+      description:
+        "Like the original Portal (2007), players solve puzzles by placing portals and teleporting between them",
+      productStatus: "used",
+      images: [
+        {
+          url: "https://upload.wikimedia.org/wikipedia/he/a/a7/Portal-2-box-art1.jpg",
+          alt: "portal 2 image1",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "nintendo",
+    game: {
+      category: ["racing"],
+      name: "mario kart 8",
+      price: 170,
+      description:
+        "Mario Kart 8 is a kart racing game; players control characters from the Mario universe to race in go-karts around a course",
+      productStatus: "new",
+      images: [
+        {
+          url: "https://upload.wikimedia.org/wikipedia/he/b/b5/MarioKart8Boxart.jpg",
+          alt: "mario kart 8 image1",
+        },
+        {
+          url: "https://www.gamereactor.eu/media/90/mariokart8_2069003.jpg",
+          alt: "mario kart 8 image2",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "nintendo",
+    game: {
+      category: ["action", "adventure"],
+      name: "The Legend of Zelda: Breath of the Wild",
+      price: 175,
+      description:
+        "Breath of the Wild is an open world action-adventure game. Players are tasked with exploring the kingdom of Hyrule while controlling Link.",
+      productStatus: "used",
+      images: [
+        {
+          url: "https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
+          alt: "Zelda image1",
+        },
+      ],
+    },
+    seller: {
+      userId: "65b0d0d49efc17d664361180",
+      firstName: "root",
+      city: "Anytown",
+      phone: "050-8123091",
+    },
+  },
+  {
+    platform: "nintendo",
+    game: {
+      category: ["platform fighter"],
+      name: "smash bros",
+      price: 200,
+      description:
+        "Super Smash Bros. Ultimate is a platform fighter for up to eight players in which characters from Nintendo games and third-party franchises fight to knock each other out of an arena",
+      productStatus: "new",
+      images: [
+        {
+          url: "https://wallpapers.com/images/hd/super-smash-bros-ultimate-heroes-jump-pcqxadvjkqc63kt4.jpg",
+          alt: "smash bros image1",
+        },
+        {
+          url: "https://www.gamereactor.eu/media/92/supersmashbros_2679253.jpg",
+          alt: "smash bros image2",
         },
       ],
     },
--- src/validation/patterns.ts
+++ src/validation/patterns.ts
@@ -2,7 +2,7 @@ const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*-])[A-Za-z\d!@#$%^&*-]{6,}$/;
 const phoneRegex =
   /^\+?(972|0)(\-)?0?(([23489]{1}\d{7})|[5]{1}(\d{1}(\-)\d{7}|\d{8}))$/;
-const platformRegex = /xbox|pc|playstation/;
+const platformRegex = /xbox|pc|playstation|nintendo/;
 const productStatusRegex = /new|like new|used/;
 
 export { passwordRegex, phoneRegex, platformRegex, productStatusRegex };
