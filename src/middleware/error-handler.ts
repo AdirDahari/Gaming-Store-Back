c7448b3e9f016cba5c76837adc530077e729cb0a
Adir Dahari <Adir10500@gmail.com>
2024-03-18 11:17:47 +0200

    fix error handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,13 +1,31 @@
 import { ErrorRequestHandler } from "express";
 import { Logger } from "../logs/logger";
+import { GameError } from "../error/gaming-store-error";
 
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
-  try {
-    Logger.error(err);
-    res.status(500).json({ message: "Internal Server Error", err });
-  } catch (e) {
-    Logger.error(e);
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  Logger.error(err);
+
+  if (err instanceof GameError) {
+    return res.status(err.status).json({ message: err.message });
   }
+
+  if (err.code && err.code == 11000 && err.keyPattern && err.keyValue) {
+    return res.status(400).json({
+      message: "Duplicate Key - Must be Unique",
+      property: err.keyValue,
+      index: err.keyPattern,
+    });
+  }
+
+  if (err instanceof SyntaxError) {
+    return res.status(400).json({ message: "Invalid Json" });
+  }
+
+  if (err.name && err.name === "JsonWebTokenError") {
+    return res.status(400).json({ message: "Invalid Token" });
+  }
+
+  return res.status(500).json({ message: "Internal Server Error", err });
 };
 
 export { errorHandler };
